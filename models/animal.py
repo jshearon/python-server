aca45531c4fd5e53fab9b905ca1b1b9d4b51f79b
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-10-20 18:54:44 -0500

    added put function for animals

--- models/animal.py
+++ models/animal.py
@@ -8,5 +8,5 @@ class Animal():
         self.name = name
         self.species = species
         self.status = status
-        self.location_id = location_id
         self.customer_id = customer_id
+        self.location_id = location_id
