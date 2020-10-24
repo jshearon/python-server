f37684833b22dc4551ea8b3a0a5dcb44ed6f3dbf
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-10-24 09:57:05 -0500

    create animal function

--- request_handler.py
+++ request_handler.py
@@ -13,6 +13,13 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
+    
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        self.end_headers()
         
     def parse_url(self, path):
         path_params = path.split("/")
